cc769af864a20cc152e024b1667ac9c62d4b6ed2
epzads <epzads@gmail.com>
2019-05-23 21:46:02 -0400

    frontend with semantic n login page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar'
+
+
+function App() {
+  return (
+    <div> 
+
+          <NavBar/> 
+    </div>
+  );
+}
+
+export default App;
