3047592565d3995a413fd95982c84e823e5dd832
Samidh Patel <spatel1345@gmail.com>
2019-05-27 17:22:36 -0400

    working on the frontend

--- src/App.js
+++ src/App.js
@@ -6,25 +6,39 @@ import NavBar from './components/NavBar'
 import Menu from './components/Menu'
 import CustomizeMenu from './components/CustomizeMenu'
 import Checkout from './components/Checkout'
+import {BrowserRouter, Router, Link} from "react-router-dom"
 
 
-class App extends Component {
 
-  handleClick= () => {
+// class App extends Component {
 
-  }
+  // handleClick= () => {
 
-  render() {
-    return (
-      <div> 
+  // }
+
+  // render() {
+  //   // return (
+  //   //   <div> 
   
-            <NavBar/>
-            <Menu/>
-            <CustomizeMenu/>
-            <Checkout />
-      </div>
-    );
-  }
-}
+  //   //         <NavBar/>
+  //   //         <Menu/>
+  //   //         <CustomizeMenu/>
+  //   //         <Checkout />
+  //   //   </div>
+  //   // );
+
+    
+  // }
+
+
+  ReactDOM.render(
+    <BrowserRouter>
+        <Route path="/" component={Menu}/>
+        <Route path="pizza/customize" component={CustomizeMenu}/>
+        <Route path="pizza/checkout" component={Checkout}/>
+    </BrowserRouter>
+  )
+
+// }
 
 export default App;
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+
 
 class Checkout extends Component {
     render() {
