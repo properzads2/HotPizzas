9e88440ded7f9ff99ddb2e5670ef703f819d2ffa
Samidh Patel <spatel1345@gmail.com>
2019-05-29 15:18:22 -0400

    finished with the place order screen

--- src/components/PlaceOrder.js
+++ src/components/PlaceOrder.js
@@ -6,7 +6,8 @@ class PlaceOrder extends Component {
         <div class="ui segment">
         <h2 class="ui centered floated header">Thank You for Placing an Order with Hot Pizza's.</h2>
         <div class="ui clearing divider"></div>
-        <p> Your order has been placed. Estimated delivery time is 20-35 mins. Thank You for visiting Hot Pizzas</p>
+        <p> Your order has been placed. Estimated delivery time is 20-35 mins. Thank You for visiting Hot Pizzas.</p>
+        <p> Payment will be processed via credit card for $ amount paid </p>
         </div>
         )
     }
