64b4801d22e73e2c2ecde16ba13b5b34592f0647
epzads <epzads@gmail.com>
2019-05-29 13:38:01 -0400

    pizza canvas ready

--- src/components/CustomizeMenu.js
+++ src/components/CustomizeMenu.js
@@ -7,10 +7,41 @@ import pizza5 from './pizza5.jpg'
 import pizza6 from './pizza6.jpg'
 import pizza7 from './pizza7.jpg'
 
+import blank from './blank.png'
+
 class CustomizeMenu extends Component {
+
+    componentDidMount() {
+        const canvas = this.refs.canvas
+        const ctx = canvas.getContext("2d")
+        const base= this.refs.cheese
+        // ctx.rect(60, 60, 300, 300);
+        // ctx.rect(60, 60, 100, 100);
+        // ctx.fillStyle = 'green';
+        // ctx.fill()
+     //   ctx.fillStyle="red";
+//ctx.fillRect(60,60,300,300);  // filled with red
+
+base.onload = () => {
+    ctx.drawImage(base, 50,60,350,350)
+    //ctx.font = "40px Courier"
+    //ctx.fillText(this.props.text, 210, 75)
+  
+
+
+// ctx.fillStyle="green";
+// ctx.fillRect(70,70,280,280);  // filled with green
+
+// ctx.fillStyle="blue";
+// ctx.fillRect(80,80,260,260);  // filled with blue
+}
+    }
+
+
+
+
     render() {
         return (
-  
         <div class="ui segment">
              <div class="ui two column very relaxed grid" style={{ backgroundImage: `url(${pizza7})`, backgroundSize: 'cover',
              backgroundRepeat: 'no-repeat' }}>
@@ -18,13 +49,13 @@ class CustomizeMenu extends Component {
              backgroundRepeat: 'no-repeat' }}> */}
             <div style={{ backgroundImage: `url(${pizza7})`, backgroundSize: 'cover',
              backgroundRepeat: 'no-repeat' }}>
+                 
                 <h1 align="center"> Customize Your Pizza </h1>
                 <h3 align="center"> Classic Pizza </h3>
-                <img src="https://www.pizzahut.com/assets/w/tile/thor/Cheese.png" align="center"/>
             <br/>
             <br/>
             <br/>
-            <div class="ui raised very padded text container segment">
+            <div class="ui raised very padded text container segment" align="top">
             <h2 align="center"> Checkout </h2>
             <h2 align="left"> Order Details</h2>
             <h2 class="ui header"> Pizza Type: CHEESE CRUST &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;  &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; $7.99</h2>
@@ -40,74 +71,81 @@ class CustomizeMenu extends Component {
                     Place Order
             </button>
             </div>
+>
+        <canvas ref="canvas"   width={400} height={600}  />
+  
         </div>
         {/* <div class="column"> */}
-        <div class="column">
+        
+        <div class="column" align="left"> 
+    
+             
+
             <h1 align="center"> Choose Toppings </h1>
             <h3 align="left"> Each Topping added is $0.50.</h3>
             <h2 align="center"> Veggies </h2>
+            
             <h3> Olives </h3>
-            <img src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Olive_black.png"/>
+            <img ref="olives" src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Olive_black.png"/>
             
             <div class="ui checkbox">
-                <input type="checkbox"  onChange={this.props.olives} name="example"></input>
+                <input type="checkbox"  onChange={(e)=>this.props.olives(e,this.refs.canvas,this.refs.olives,this.refs.blank)} name="example"></input>
                 <label> Select </label>
                 </div>
             
             <h3> Diced Tomatoes </h3>
-            <img src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Tomato.png"/>
+            <img ref="tomatoes"src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Tomato.png"/>
             
             <div class="ui checkbox">
-                <input type="checkbox" onChange={this.props.tomatoes} name="example"></input>
+                <input type="checkbox" onChange={(e)=>this.props.tomatoes(e,this.refs.canvas,this.refs.tomatoes)} name="example"></input>
                 <label> Select </label>
                 </div>
 
             <h3> Red Onions </h3>
-            <img src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Onion_Red.png"/>
-            
+            <img ref="onions"src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Onion_Red.png"/>
             <div class="ui checkbox">
-                <input type="checkbox" onChange={this.props.onions} name="example"></input>
+                <input type="checkbox" onChange={(e)=>this.props.onions(e,this.refs.canvas,this.refs.onions)} name="example"></input>
                 <label> Select </label>
                 </div>
 
             <h3> Green Peppers </h3>
-            <img src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_GreenBellPepper.png"/>
+            <img ref="green" src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_GreenBellPepper.png"/>
             
             <div class="ui checkbox">
-                <input type="checkbox" onChange={this.props.greenpeper} name="example"></input>
+                <input type="checkbox" onChange={(e)=>this.props.greenpeper(e,this.refs.canvas,this.refs.green)} name="example"></input>
                 <label> Select </label>
                 </div>            
             
             <h2 align="center"> Meat </h2>
             <h3> Pepperoni </h3>
-            <img src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Pepperoni.png"/>
+            <img ref="peperoni" src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Pepperoni.png"/>
             
             <div class="ui checkbox">
-                <input type="checkbox" onChange={this.props.peperoni} name="example"></input>
+                <input type="checkbox" onChange={(e)=>this.props.peperoni(e,this.refs.canvas,this.refs.peperoni)} name="example"></input>
                 <label> Select </label>
                 </div>
 
             <h3> Sausage </h3>
-            <img src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_ItalianSausage.png"/>
+            <img ref="sausage" src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_ItalianSausage.png"/>
             
             <div class="ui checkbox">
-                <input type="checkbox" onChange={this.props.sausage} name="example"></input>
+                <input type="checkbox" onChange={(e)=>this.props.sausage(e,this.refs.canvas,this.refs.sausage)} name="example"></input>
                 <label> Select </label>
                 </div>
 
             <h3> Beef </h3>
-            <img src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Beef.png"/>
+            <img ref="beef" src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Beef.png"/>
             
             <div class="ui checkbox">
-                <input type="checkbox" onChange={this.props.beef} name="example"></input>
+                <input type="checkbox" onChange={(e)=>this.props.beef(e,this.refs.canvas,this.refs.beef)} name="example"></input>
                 <label> Select </label>
                 </div>
 
             <h3> Chicken </h3>
-            <img src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Pork.png"/>
+            <img ref="chicken" src="https://www.pizzahut.com/assets/w/evo_pb/sidebar/icon_topping_Pork.png"/>
             
             <div class="ui checkbox">
-                <input type="checkbox" onChange={this.props.chicken} name="example"></input>
+                <input type="checkbox" onChange={(e)=>this.props.chicken(e,this.refs.canvas,this.refs.chicken)} name="example"></input>
                 <label> Select </label>
                 </div>
 
@@ -115,6 +153,11 @@ class CustomizeMenu extends Component {
   
         </div>
         <div class="ui vertical divider">
+
+         <img ref="cheese" src="https://www.pizzahut.com/assets/w/tile/thor/Cheese.png"  height="1" width="1" align="left"/>
+         <img ref="blank" src={blank}  align="left"/>
+
+
         </div>
     </div>
     
